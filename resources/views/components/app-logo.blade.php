15359571c6b54df0cea209ad8a77ff7db5420588
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-12-17 19:20:32 +0800

    fix: correct spelling of 'Permatadhi Swara' in app logo component

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,5 +3,5 @@
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div> --}}
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Permathadi Swara</span>
+    <span class="mb-0.5 truncate leading-tight font-semibold">Permatadhi Swara</span>
 </div>
